1bfd41a5a4f6ea35661d55bbd31651bc08188483
Parag Dhali <paragmmbeta@gmail.com>
2025-12-31 17:59:39 +0530

    Special routes making

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,19 @@
-// import Image from "next/image";
-
+"use client";
 import Menu from "./components/Menu";
+import {useRouter} from 'next/navigation';
 
 export default function Home() {
+  const router=useRouter();
+  const Goto =()=>{
+    router.push('/about');
+  }
+
   return (
     <div>
       <Menu />
       <p>Home</p>
+      <button onClick={Goto}>Go to About</button>
+      <button onClick={()=>router.push('/api')}>Go to API e</button>
     </div>
   );
 }
