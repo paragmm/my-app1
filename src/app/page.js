d992f90995e6fb63717f7da7340e8fb554d587f4
Parag Dhali <paragmmbeta@gmail.com>
2025-12-31 18:04:13 +0530

    Replace route

--- src/app/page.js
+++ src/app/page.js
@@ -8,12 +8,17 @@ export default function Home() {
     router.push('/about');
   }
 
+  const GotoReplace =()=>{
+    router.push('/about');
+  }
+
   return (
     <div>
       <Menu />
       <p>Home</p>
       <button onClick={Goto}>Go to About</button>
       <button onClick={()=>router.push('/api')}>Go to API e</button>
+      <button onClick={GotoReplace}>Go to About and Replace</button>
     </div>
   );
 }
