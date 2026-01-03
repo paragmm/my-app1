a4829abd8f17f83c29dedf99ba8a1c71159ba1af
Parag <paragmmbeta@gmail.com>
2026-01-03 22:49:48 +0530

    3rd party image domain allow

--- next.config.mjs
+++ next.config.mjs
@@ -4,17 +4,32 @@ const nextConfig = {
   turbopack: {
     root: "D:/NextJSApps/my-app1",
   },
+  distDir: 'build',
+  poweredByHeader: false,
+  images: {
+    domains: ['images.unsplash.com', 'avatars.githubusercontent.com'],
+  },
+
   async headers() {
     return [
       {
         source: '/profile',
         headers: [
-          {
-            key: 'app-auth-key',
-            value: 'patah12345',
-          },
+          { key: 'app-auth-key', value: 'patah12345' }
         ],
       },
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'X-Content-Type-Options', value: 'nosniff'},
+          { key: 'X-Frame-Options', value: 'DENY'},
+          // { key: 'Content-Security-Policy', value: 'same-origin'},
+          { key: 'X-Content-Type-Options', value: 'nosniff' },
+          { key: 'Referrer-Policy', value: 'strict-origin-when-cross-origin' },
+          // { key: 'Permissions-Policy', value: "camera=(), microphone=(), geolocation=(), payment=(), usb=()" },
+          { key: 'X-XSS-Protection', value: "1; mode=block" },
+        ]
+      }
     ];
   },
 };
