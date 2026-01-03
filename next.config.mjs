b52242252ff7b7b46a420a7d8faabbcbba4865f1
Parag <paragmmbeta@gmail.com>
2026-01-03 21:50:12 +0530

    Header config added for /profile page oly

--- next.config.mjs
+++ next.config.mjs
@@ -3,7 +3,20 @@ const nextConfig = {
   /* config options here */
   turbopack: {
     root: "D:/NextJSApps/my-app1",
-  }
+  },
+  async headers() {
+    return [
+      {
+        source: '/profile',
+        headers: [
+          {
+            key: 'app-auth-key',
+            value: 'patah12345',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
